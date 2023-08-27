4bcaf7822dbe4a2a0c0eeb83b2c3da16fbac012b
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-27 19:26:03 +0530

    all links corrected, images added, content added

--- flydeck/courses/urls.py
+++ flydeck/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.courses, name='courses'),
+    path('course-details/', views.course_details, name='course-details'),
 ] 
--- flydeck/courses/views.py
+++ flydeck/courses/views.py
@@ -4,3 +4,6 @@ from . import views
 # Create your views here.
 def courses(request):
     return render(request, 'courses.html')
+
+def course_details(request):
+    return render(request, 'course-details.html')
